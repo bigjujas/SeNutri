2dff0e4bb003008851a5fdd56c36f0e4c3e9dc79
bigjujas <jpsgamerz@gmail.com>
2024-12-02 12:18:46 -0300

    so falta o json-server

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Pratos from "./Pratos"
@@ -26,9 +26,27 @@ export default function App() {
 function AppContent() {
   return (
     <>
-    <div className="home_tab">
-      <h1>Aqui é a home fml</h1>
-    </div>
+      <section className="banner_principal">
+        <div className="texto__banner">
+          <p className="paragrafo__banner">Bem-vindo à SeNutri! Somos apaixonados por saúde e bem-estar. Nosso objetivo é simplificar sua jornada rumo a uma vida mais saudável, conectando você a soluções personalizadas e práticas que fazem a diferença no seu dia a dia.
+          </p>
+          <h1 className="conhecer__button"><Link draggable="false" to="/pratos">Conhecer pratos</Link></h1>
+        </div>
+        <img src="./imagem.png" alt="" />
+      </section>
+      <section className="main__container">
+        <div className="texto__main">
+          <h1 className="titulo__main">Casos de Sucesso</h1>
+          <p>Conheça nossos casos de sucesso! Aqui, você confere histórias inspiradoras de pessoas que transformaram suas rotinas e alcançaram suas metas de saúde com a ajuda da SeNutri. São relatos reais que mostram como nossa plataforma pode fazer a diferença na sua jornada!
+          </p>
+        </div>
+        <div className="card__pessoa">
+          <img src="./pessoa.png" alt="" />
+          <p>Marcelo Moreno transformou sua alimentação e atingiu um peso saudável com os planos personalizados da SeNutri. Hoje, vive com mais equilíbrio e disposição!</p>
+
+          <h3 className="nome__card">Marcelo Moreno - 34 Anos</h3>
+        </div>
+      </section>
     </>
   )
 }
